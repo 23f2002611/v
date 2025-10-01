ddbf455d36d5ab4119e752ff71c05dbc3153cb03
23f2002611 <23f2002611@ds.study.iitm.ac.in>
2025-10-01 23:17:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.latency import app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
